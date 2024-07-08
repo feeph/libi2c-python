5c85f6b691384ecc1e9aee7f635a86a2d3a1dbc7
Feeph Aifeimei <55798703+feeph@users.noreply.github.com>
2024-07-08 19:08:01 +0000

    feat: provide a simulated I²C bus (#1)

---
+++ src/feeph/i2c/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""
+"""
+
+# the following imports are provided for user convenience
+# flake8: noqa: F401
+from feeph.i2c.bus import I2cBus, SimulatedI2cBus
+from feeph.i2c.device import I2cDevice
