efe5a251258c87f6032fc3a1ce1fe9c7e2de21d6
Feeph Aifeimei <55798703+feeph@users.noreply.github.com>
2024-07-08 22:14:21 +0200

    fix: update 'feeph/i2c/__init__.py' with correct import

--- src/feeph/i2c/__init__.py
+++ src/feeph/i2c/__init__.py
@@ -4,5 +4,5 @@
 
 # the following imports are provided for user convenience
 # flake8: noqa: F401
-from feeph.i2c.bus import I2cBus, SimulatedI2cBus
+from feeph.i2c.bus import HardwareI2cBus, SimulatedI2cBus
 from feeph.i2c.device import I2cDevice
